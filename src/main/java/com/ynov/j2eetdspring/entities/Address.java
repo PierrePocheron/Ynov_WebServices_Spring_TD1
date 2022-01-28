47a1732ae225a4797e4bcd551dc03294bd0e44b9
PierrePocheron <pierre.pocheron@gmail.com>
2022-01-28 15:24:29 +0100

    Feat: add entitiy Address

---
+++ src/main/java/com/ynov/j2eetdspring/entities/Address.java
@@ -0,0 +1,107 @@
+package com.ynov.j2eetdspring.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "address")
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_address")
+    private Long id;
+
+    @Column(name = "num")
+    private Long num;
+
+    @Column(name = "street")
+    private String street;
+
+    @Column(name = "additional")
+    private String additional;
+
+    @Column(name = "city")
+    private String city;
+
+    @Column(name = "country")
+    private String country;
+
+    public Address(){}
+
+    public Address(Long id, Long num, String street, String additional, String city, String country) {
+        this.id = id;
+        this.num = num;
+        this.street = street;
+        this.additional = additional;
+        this.city = city;
+        this.country = country;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getNum() {
+        return num;
+    }
+
+    public void setNum(Long num) {
+        this.num = num;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getAdditional() {
+        return additional;
+    }
+
+    public void setAdditional(String additional) {
+        this.additional = additional;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
