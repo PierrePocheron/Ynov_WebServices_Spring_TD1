4e5bd980b5a8f81664b96bd36f1c61a976cf94a4
PierrePocheron <pierre.pocheron@gmail.com>
2022-01-28 15:23:24 +0100

    Initial commit

---
+++ src/main/java/com/ynov/j2eetdspring/J2eeTdSpringApplication.java
@@ -0,0 +1,13 @@
+package com.ynov.j2eetdspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class J2eeTdSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(J2eeTdSpringApplication.class, args);
+    }
+
+}
